efef5e64a3c08f9e86be76572ade818be0b4f62a
yellowmix <yellowmix@gmail.com>
2017-02-28 16:17:47 -0500

    Add Logging, External Configuration File, Minor Readability Changes
    logging module is used to provide two levels of output, INFO and DEBUG,
    the latter being verbose within the counting loop.
    
    configparser is used to enable use of an external configuration file.
    This makes it easier to configure more options, such as output filename.
    
    Set membership test on line 35 is made more explicit.
    
    While 'flair' and 'x' variables are out of each others' scope, 'flair'
    replaces local variable name 'x' to avoid confusion.

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import praw
 import io
 import json
+import logging.config
+import configparser
+import os
+import sys
 
 
-r = praw.Reddit(client_id='CLIENT_ID', 
-                client_secret='CLIENT_SECRET', 
-                password='PASSWORD', 
-                user_agent='subreddit flair distribution counter by /u/zebradolphin5', 
-                username='USER')
-#r.login(user, passw, disable_warning=True)
-subreddit = r.subreddit("SUBREDDIT_NAME")
+config = configparser.ConfigParser()
+configfile_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+configfile_path = os.path.join(configfile_path, 'Subreddit-Flair-Distribution-Counter.cfg')
+config.read(configfile_path)
+logging.config.fileConfig(configfile_path)
+
+while True:
+    try:
+        r = praw.Reddit(client_id=config['reddit']['client_id'],
+                        client_secret=config['reddit']['client_secret'],
+                        user_agent=config['reddit']['user_agent'],
+                        username=config['reddit']['username'],
+                        password=config['reddit']['password'])
+        logging.info('Logged in as {0}.'.format(r.user.me()))
+        break
+    except Exception as e:
+        logging.error('ERROR: {0}'.format(e))
+
+subreddit = r.subreddit(config['reddit']['subreddit'])
 
 flairs = {}
-for x in subreddit.flair(limit=None):
-    flairName = x['flair_css_class']
-    if not flairName in flairs:
+
+logging.debug('Counting flairs...')
+for flair in subreddit.flair(limit=None):
+    flairName = flair['flair_css_class']
+    if flairName not in flairs:
         flairs[flairName] = 0
+        logging.debug('Flair added: {0}'.format(flairName))
 
     flairs[flairName] += 1
+    logging.debug('Flair counted: {0}'.format(flairName))
 
+logging.info('Sorting flairs...')
 sorted_flairs = sorted(flairs.items(), key=lambda x: x[1], reverse=True)
-with io.open('flaircount.json', 'w+', encoding='utf-8') as f:
-	f.write(str(json.dumps(sorted_flairs, ensure_ascii=False, indent=4, separators=(',', ': '))))    
+logging.info('Writing flairs...')
+with io.open(config['output']['filename'], 'w+', encoding='utf-8') as f:
+    f.write(str(json.dumps(sorted_flairs, ensure_ascii=False, indent=4, separators=(',', ': '))))
+logging.info('Done!')
